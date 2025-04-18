45359701201d0ed2d9edf5bed59c48390b45f03f
Gwynne Raskind <gwynne.raskind@prism-ipx.com>
2025-04-17 22:43:28 -0500

    import

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:6.1
+import PackageDescription
+
+let package = Package(
+    name: "swift-async-algorithms-temp-extras",
+    platforms: [
+        .macOS(.v10_15),
+    ],
+    products: [
+        .library(name: "MultiProducerSingleConsumerChannel", targets: ["MultiProducerSingleConsumerChannel"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "MultiProducerSingleConsumerChannel",
+            dependencies: [
+                .product(name: "OrderedCollections", package: "swift-collections"),
+                .product(name: "DequeModule", package: "swift-collections"),
+            ]
+        ),
+        .testTarget(
+            name: "MultiProducerSingleConsumerChannelTests",
+            dependencies: [
+                .target(name: "MultiProducerSingleConsumerChannel"),
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v5]
+)
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a temporary repo that holds a copy of MultiConsumerSingleConsumerChannel because it needs to be built in Swift 5 language mode in order to work at the moment.
